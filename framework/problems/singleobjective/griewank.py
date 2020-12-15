436cfb3e9e51858931a03c124bb03d94ae6de5b1
fruktus <fruktusek@gmail.com>
2020-12-15 01:53:54 +0100

    implemented griewank benchmark function (available from framework module)

---
+++ framework/problems/singleobjective/griewank.py
@@ -0,0 +1,36 @@
+from math import sqrt, cos
+
+from jmetal.core.problem import FloatProblem
+from jmetal.core.solution import FloatSolution
+
+
+class Griewank(FloatProblem):
+
+    def __init__(self, number_of_variables: int = 10):
+        super(Griewank, self).__init__()
+        self.number_of_objectives = 1
+        self.number_of_variables = number_of_variables
+        self.number_of_constraints = 0
+
+        self.obj_directions = [self.MINIMIZE]
+        self.obj_labels = ['f(x)']
+
+        self.lower_bound = [-5.12 for _ in range(number_of_variables)]
+        self.upper_bound = [5.12 for _ in range(number_of_variables)]
+
+        FloatSolution.lower_bound = self.lower_bound
+        FloatSolution.upper_bound = self.upper_bound
+
+    def evaluate(self, solution: FloatSolution) -> FloatSolution:
+        sum = 0
+        for x in solution.variables:
+            sum += x * x
+        product = 1
+        for i in range(len(solution.variables)):
+            product *= cos(solution.variables[i] / sqrt(i + 1))
+
+        solution.objectives[0] = 1 + sum / 4000 - product
+        return solution
+
+    def get_name(self) -> str:
+        return 'Griewank'
