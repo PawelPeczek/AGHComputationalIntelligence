647532b153ac7dac2caf1988127c983f79427706
Busiu <busiu69@gmail.com>
2020-12-29 04:12:27 +0100

    Try to fix slow pace of griewank_runner

--- differential_evolution/griewank_runner.py
+++ differential_evolution/griewank_runner.py
@@ -5,181 +5,47 @@ from jmetal.operator import PolynomialMutation
 from jmetal.util.termination_criterion import StoppingByEvaluations
 
 from differential_evolution.de import DifferentialEvolution
-from clonal_selection.de_jong_1 import DeJong1
-from clonal_selection.de_jong_5 import DeJong5
 from framework.config import RESULTS_DIR
 from framework.runner.multi_algorithm_runner import ExecutionUnit, MultiAlgorithmRunner, save_execution_history
 from framework.problems.singleobjective.griewank import Griewank
 
+def get_execution_unit(number_of_partners, iters) -> ExecutionUnit:
+    execution_unit = ExecutionUnit(
+        algorithm_cls=DifferentialEvolution,
+        problem_name= f"Griewank_100dim_{number_of_partners}_partners"
+    )
+
+    griewank_problem = Griewank(number_of_variables=100, lower_bound=-100, upper_bound=100)
+    
+    for _ in range(iters):
+        execution_unit.register_run(
+            parameters={
+            "problem": griewank_problem,
+            "each_species_size": 50,
+            "number_of_partners": number_of_partners,
+            "cr": 0.9, "f": 0.8,
+            "max_iter": 250
+            }
+        )
+    
+    return execution_unit
 
 def run() -> None:
     target_path = os.path.join(RESULTS_DIR, "testDE_Griewank.json")
-    griewank_problem = Griewank(number_of_variables=100, lower_bound=-100, upper_bound=100)
 
-    variable_species_size_execution_unit = ExecutionUnit(
-        algorithm_cls=DifferentialEvolution,
-        problem_name="Griewank_100dim_variable_species_size"
-    ).register_run(
-        parameters={
-            "problem": griewank_problem,
-            "each_species_size": 1,
-            "cr": 0.9, "f": 0.8,
-            "max_iter": 500
-        }
-     ).register_run(
-        parameters={
-            "problem": griewank_problem,
-            "each_species_size": 10,
-            "cr": 0.9, "f": 0.8,
-            "max_iter": 500
-        }
-    ).register_run(
-        parameters={
-            "problem": griewank_problem,
-            "each_species_size": 20,
-            "cr": 0.9, "f": 0.8,
-            "max_iter": 500
-        },
-    ).register_run(
-        parameters={
-            "problem": griewank_problem,
-            "each_species_size": 50,
-            "cr": 0.9, "f": 0.8,
-            "max_iter": 500
-        },
-    ).register_run(
-        parameters={
-            "problem": griewank_problem,
-            "each_species_size": 100,
-            "cr": 0.9, "f": 0.8,
-            "max_iter": 500
-        },
-    )
-
-    variable_number_of_species_execution_unit = ExecutionUnit(
-        algorithm_cls=DifferentialEvolution,
-        problem_name="Griewank_100dim_variable_number_of_species"
-    ).register_run(
-        parameters={
-            "problem": griewank_problem,
-            "each_species_size": 10,
-            "no_species": 1,
-            "cr": 0.9, "f": 0.8,
-            "max_iter": 500
-        }
-    ).register_run(
-        parameters={
-            "problem": griewank_problem,
-            "each_species_size": 10,
-            "no_species": 10,   # default
-            "cr": 0.9, "f": 0.8,
-            "max_iter": 500
-        }
-    ).register_run(
-        parameters={
-            "problem": griewank_problem,
-            "each_species_size": 10,
-            "no_species": 20,
-            "cr": 0.9, "f": 0.8,
-            "max_iter": 500
-        }
-    ).register_run(
-        parameters={
-            "problem": griewank_problem,
-            "each_species_size": 10,
-            "no_species": 50,
-            "cr": 0.9, "f": 0.8,
-            "max_iter": 500
-        }
-    ).register_run(
-        parameters={
-            "problem": griewank_problem,
-            "each_species_size": 10,
-            "no_species": 100,
-            "cr": 0.9, "f": 0.8,
-            "max_iter": 500
-        }
-    )
-
-    variable_number_of_partners_execution_unit = ExecutionUnit(
-        algorithm_cls=DifferentialEvolution,
-        problem_name="Griewank_100dim_variable_number_of_partners"
-    ).register_run(
-        parameters={
-            "problem": griewank_problem,
-            "each_species_size": 10,
-            "number_of_partners": 3,   # default
-            "cr": 0.9, "f": 0.8,
-            "max_iter": 500
-        }
-    ).register_run(
-        parameters={
-            "problem": griewank_problem,
-            "each_species_size": 10,
-            "number_of_partners": 5,
-            "cr": 0.9, "f": 0.8,
-            "max_iter": 500
-        }
-    ).register_run(
-        parameters={
-            "problem": griewank_problem,
-            "each_species_size": 10,
-            "number_of_partners": 10,
-            "cr": 0.9, "f": 0.8,
-            "max_iter": 500
-        }
-    ).register_run(
-        parameters={
-            "problem": griewank_problem,
-            "each_species_size": 10,
-            "number_of_partners": 15,
-            "cr": 0.9, "f": 0.8,
-            "max_iter": 500
-        }
-    ).register_run(
-        parameters={
-            "problem": griewank_problem,
-            "each_species_size": 10,
-            "number_of_partners": 20,
-            "cr": 0.9, "f": 0.8,
-            "max_iter": 500
-        }
-    )
-
-    variable_iterations_execution_unit = ExecutionUnit(
-        algorithm_cls=DifferentialEvolution,
-        problem_name="Griewank_100dim_variable_max_iter"
-    ).register_run(
-        parameters={
-            "problem": griewank_problem,
-            "each_species_size": 20,
-            "no_species": 50,
-            "cr": 0.9, "f": 0.8,
-            "max_iter": 250
-        }
-    ).register_run(
-        parameters={
-            "problem": griewank_problem,
-            "each_species_size": 20,
-            "no_species": 50,
-            "cr": 0.9, "f": 0.8,
-            "max_iter": 500
-        }
-    ).register_run(
-        parameters={
-            "problem": griewank_problem,
-            "each_species_size": 20,
-            "no_species": 50,
-            "cr": 0.9, "f": 0.8,
-            "max_iter": 1000
-        }
-    )
+    execution_unit_3_partners = get_execution_unit(3, 1)
+    execution_unit_5_partners = get_execution_unit(5, 1)
+    execution_unit_7_partners = get_execution_unit(7, 1)
+    execution_unit_10_partners = get_execution_unit(10, 1)
 
 
 
     runner = MultiAlgorithmRunner(
         execution_units=[
-            variable_species_size_execution_unit, variable_number_of_species_execution_unit, variable_number_of_partners_execution_unit, variable_iterations_execution_unit
+            execution_unit_3_partners,
+            execution_unit_5_partners,
+            execution_unit_7_partners,
+            execution_unit_10_partners
         ]
     )
 
